1df989200b0804618e2dce7069899a46cd42dc36
Chandan B <116443036+Chandanb2003@users.noreply.github.com>
2026-02-03 16:17:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,13 +20,17 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "tetris-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_route_table" "rt" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc.id
 
   route {
@@ -35,6 +39,9 @@ resource "aws_route_table" "rt" {
   }
 }
 
+############################
+# PUBLIC SUBNETS (ELB READY)
+############################
 resource "aws_subnet" "a" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -42,7 +49,9 @@ resource "aws_subnet" "a" {
   map_public_ip_on_launch = true
 
   tags = {
-    "kubernetes.io/cluster/free-eks" = "shared"
+    Name                                = "tetris-subnet-a"
+    "kubernetes.io/cluster/tetris-eks" = "shared"
+    "kubernetes.io/role/elb"           = "1"
   }
 }
 
@@ -53,25 +62,27 @@ resource "aws_subnet" "b" {
   map_public_ip_on_launch = true
 
   tags = {
-    "kubernetes.io/cluster/free-eks" = "shared"
+    Name                                = "tetris-subnet-b"
+    "kubernetes.io/cluster/tetris-eks" = "shared"
+    "kubernetes.io/role/elb"           = "1"
   }
 }
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.a.id
-  route_table_id = aws_route_table.rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.b.id
-  route_table_id = aws_route_table.rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 ############################
 # IAM ROLES
 ############################
 resource "aws_iam_role" "cluster_role" {
-  name = "free-eks-cluster-role"
+  name = "tetris-eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -89,7 +100,7 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
 }
 
 resource "aws_iam_role" "node_role" {
-  name = "free-eks-node-role"
+  name = "tetris-eks-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -124,10 +135,9 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.cluster_role.arn
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.a.id,
-      aws_subnet.b.id
-    ]
+    subnet_ids              = [aws_subnet.a.id, aws_subnet.b.id]
+    endpoint_public_access  = true
+    endpoint_private_access = false
   }
 
   depends_on = [
@@ -136,16 +146,13 @@ resource "aws_eks_cluster" "eks" {
 }
 
 ############################
-# NODE GROUP (FREE TIER FRIENDLY)
+# NODE GROUP
 ############################
 resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "main"
   node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = [
-    aws_subnet.a.id,
-    aws_subnet.b.id
-  ]
+  subnet_ids      = [aws_subnet.a.id, aws_subnet.b.id]
 
   instance_types = ["t3.small"]
 
@@ -154,4 +161,10 @@ resource "aws_eks_node_group" "nodes" {
     min_size     = 2
     max_size     = 2
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_cni,
+    aws_iam_role_policy_attachment.node_ecr
+  ]
 }
