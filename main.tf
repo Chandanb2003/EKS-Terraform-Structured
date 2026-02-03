8fd6c5f878d791d6b613a48863d50b3ec408799f
Chandan B <116443036+Chandanb2003@users.noreply.github.com>
2026-02-03 16:15:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
@@ -5,51 +16,18 @@ provider "aws" {
 ############################
 # VPC
 ############################
-resource "aws_vpc" "eks_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
 }
 
-############################
-# SUBNETS (2 AZs)
-############################
-resource "aws_subnet" "subnet_a" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "eks-subnet-a"
-  }
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
-
-  tags = {
-    Name = "eks-subnet-b"
-  }
-}
-
-############################
-# INTERNET GATEWAY
-############################
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.vpc.id
 }
 
-############################
-# ROUTE TABLE
-############################
 resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -57,21 +35,43 @@ resource "aws_route_table" "rt" {
   }
 }
 
+resource "aws_subnet" "a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/cluster/free-eks" = "shared"
+  }
+}
+
+resource "aws_subnet" "b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/cluster/free-eks" = "shared"
+  }
+}
+
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet_a.id
+  subnet_id      = aws_subnet.a.id
   route_table_id = aws_route_table.rt.id
 }
 
 resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.subnet_b.id
+  subnet_id      = aws_subnet.b.id
   route_table_id = aws_route_table.rt.id
 }
 
 ############################
-# EKS CLUSTER IAM ROLE
+# IAM ROLES
 ############################
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+resource "aws_iam_role" "cluster_role" {
+  name = "free-eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -83,35 +83,13 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-############################
-# EKS CLUSTER
-############################
-resource "aws_eks_cluster" "eks" {
-  name     = "demo-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.subnet_a.id,
-      aws_subnet.subnet_b.id
-    ]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-############################
-# NODE GROUP IAM ROLE
-############################
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
+resource "aws_iam_role" "node_role" {
+  name = "free-eks-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -123,39 +101,57 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_node_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ])
+resource "aws_iam_role_policy_attachment" "node_worker" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
 
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = each.value
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_ecr" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 ############################
-# NODE POOL (3 NODES)
+# EKS CLUSTER
 ############################
-resource "aws_eks_node_group" "main_pool" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "main-pool"
-  node_role_arn  = aws_iam_role.eks_node_role.arn
+resource "aws_eks_cluster" "eks" {
+  name     = "tetris-eks"
+  role_arn = aws_iam_role.cluster_role.arn
 
-  subnet_ids = [
-    aws_subnet.subnet_a.id,
-    aws_subnet.subnet_b.id
-  ]
-
-  scaling_config {
-    desired_size = 3
-    min_size     = 3
-    max_size     = 4
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.a.id,
+      aws_subnet.b.id
+    ]
   }
 
-  instance_types = ["t3.medium"]
-
   depends_on = [
-    aws_eks_cluster.eks
+    aws_iam_role_policy_attachment.cluster_policy
   ]
+}
+
+############################
+# NODE GROUP (FREE TIER FRIENDLY)
+############################
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "main"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = [
+    aws_subnet.a.id,
+    aws_subnet.b.id
+  ]
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 2
+    max_size     = 2
+  }
 }
