ca8f91ccfb537cf357e98dfaedb39ab5924861f7
Chandan B <116443036+Chandanb2003@users.noreply.github.com>
2026-02-19 11:37:45 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = aws_eks_cluster.eks.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
-output "deployer_role_arn" {
-  value = aws_iam_role.eks_deployer.arn
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.nodes.node_group_name
 }
